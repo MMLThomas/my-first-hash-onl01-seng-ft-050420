c98905a95cbb80ee9510235c82089bcc7026f681
michael thomas <michael.thomas2014@gmail.com>
2020-05-11 12:19:31 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  your_hash = {"home" => "Morocco"
+  your_hash = {"home" => "Morocco", "Ocean" => "Atlantic", "Season" => "Summer"}
 
   
 end
